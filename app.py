9e4767ea7260503af09f5ef6f1d2cb5cebfe9599
Rahul Nakka <rahulnakka55@gmail.com>
2025-08-15 14:16:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -104,5 +104,8 @@ def predict():
     
     return render_template('index.html', show_prediction=False)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
